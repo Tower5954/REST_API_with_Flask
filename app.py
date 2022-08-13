a6449f352607343ac8f216203d7e5d1d9412bbb1
Grant Robert Thompson <58478508+Tower5954@users.noreply.github.com>
2022-08-13 23:09:57 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+stores = [
+    {
+        'name': 'My store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
+
+# POST - Used to receive data
+# Get - Used to send data back only
+
+
+# POST /store data: {name}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')     # 'http://127.0.0.1:5000/store/some_name'
+def get_store(name):
+    pass
+
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    pass
+
+
+# POST /store/<string:name>/item
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_item_in_store():
+    pass
+
+
+app.run(port=5000)
